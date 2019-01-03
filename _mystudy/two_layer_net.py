a2a5e4427219dd42091a375fcb5ffe0d8dbb3c06
cloudallera <cloudallera@gmail.com>
2019-01-03 13:40:59 +0900

    Chapter5 완료

--- _mystudy/two_layer_net.py
+++ _mystudy/two_layer_net.py
@@ -1,7 +1,10 @@
+# 오차역전파법을 적용한 2층 신경망 구현
 import sys, os
 sys.path.append(os.pardir)
-from common.functions import *
+import numpy as np
+from common.layers import *
 from common.gradient import numerical_gradient
+from collections import OrderedDict
 
 
 class TwoLayerNet:
@@ -20,26 +23,27 @@ class TwoLayerNet:
             hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
 
+        # 계층 생성
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        self.lastLayer = SoftmaxWithLoss()
+
     def predict(self, x):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
+        for layer in self.layers.values():
+            x = layer.forward(x)
 
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
-
-        return y
+        return x
 
     def loss(self, x, t):    # x는 입력 데이터, t는 정답 레이블
-        y = self.predict(x)
-
-        return cross_entropy_error(y, t)
+        y = self.predict()
+        return self.lastLayer.forward(x, t)
 
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis=1)
-        t = np.argmax(t, axis=1)
+        if t.ndim != 1: t = np.argmax(t, axis=1)
 
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
@@ -55,27 +59,24 @@ class TwoLayerNet:
 
         return grads
 
-    def gradient(self, x, t):   # chapter5 에서 만들 메서드 (numerical_gradient 메서드 개선 버전)
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
+    def gradient(self, x, t):
+        # 순전파
+        self.loss(x, t)
+
+        # 역전파
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 결과 저장
         grads = {}
-
-        batch_num = x.shape[0]
-
-        # forward
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
-
-        # backward
-        dy = (y - t) / batch_num
-        grads['W2'] = np.dot(z1.T, dy)
-        grads['b2'] = np.sum(dy, axis=0)
-
-        da1 = np.dot(dy, W2.T)
-        dz1 = sigmoid_grad(a1) * da1
-        grads['W1'] = np.dot(x.T, dz1)
-        grads['b1'] = np.sum(dz1, axis=0)
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
 
         return grads
